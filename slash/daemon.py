2b7a3fcc5a83b8f812fc3d3740e83a73d2868a49
whywhy <43395692+why-in-Shanghaitech@users.noreply.github.com>
2024-12-19 16:01:33 +0800

    feat: move daemon to a seperate file

---
+++ slash/daemon.py
@@ -0,0 +1,190 @@
+import json
+import os
+import re
+import signal
+import sys
+import time
+from pathlib import Path
+from typing import List
+
+import filelock
+
+import slash.utils as utils
+from slash.core import WORK_DIR, EnvsManager, ServiceManager
+
+
+logger = utils.logger
+
+
+class DaemonPid:
+    def __init__(self, name: str):
+        self.name = name
+
+    @property
+    def deamon_pid(self) -> Path:
+        return WORK_DIR / 'daemon.pid'
+
+    @property
+    def pid(self) -> int:
+        with filelock.SoftFileLock(self.deamon_pid.with_suffix('.lock')):
+            if self.deamon_pid.exists():
+                with open(self.deamon_pid, 'r') as f:
+                    data = json.load(f)
+                return data.get(self.name, None)
+            else:
+                with open(self.deamon_pid, 'w') as f:
+                    json.dump({}, f)
+        return None
+
+    @pid.setter
+    def pid(self, pid: int):
+        with filelock.SoftFileLock(self.deamon_pid.with_suffix('.lock')):
+            with open(self.deamon_pid, 'r') as f:
+                data = json.load(f)
+            data[self.name] = pid
+            with open(self.deamon_pid, 'w') as f:
+                json.dump(data, f)
+
+    @pid.deleter
+    def pid(self):
+        with filelock.SoftFileLock(self.deamon_pid.with_suffix('.lock')):
+            with open(self.deamon_pid, 'r') as f:
+                data = json.load(f)
+            if self.name in data:
+                del data[self.name]
+            with open(self.deamon_pid, 'w') as f:
+                json.dump(data, f)
+
+    @property
+    def is_running(self):
+        if not (ret := self.pid is not None and (Path('/proc') / str(self.pid)).exists()):
+            del self.pid
+        return ret
+
+
+class Daemon:
+    """
+    The abstract daemon class that manages the services.
+
+    The daemon will be started when the slash object is created, it will take down the pid of the
+    current process; after the process exits, the daemon will check the jobs corresponding to the
+    its type (shell, with, ...) and remove the dead ones every 60 seconds. If there are no jobs left,
+    the daemon will exit.
+    """
+    # the identifier of the daemon, should be unique
+    name = None
+    # the loop interval (seconds)
+    interval = 60
+
+    def __init__(self):
+        self.envs_manager = EnvsManager()
+        self.service_manager = ServiceManager(self.envs_manager)
+        self.deamonpid = DaemonPid(self.name)
+
+    def launch_command(self) -> List[str]:
+        """
+        Get the command to launch the daemon. Should be implemented by the subclass.
+        """
+        raise NotImplementedError
+
+    def getid(self, job: str) -> str:
+        """
+        Get the unique identifier of the job. It will be passed to the validate method to check if the job is dead.
+        If the job is beyond the control of the daemon, return None. Should be implemented by the subclass.
+        """
+        raise NotImplementedError
+
+    def validate(self, jid: str) -> bool:
+        """
+        Validate the existence of a job. Should be implemented by the subclass.
+        """
+        raise NotImplementedError
+
+    def start(self):
+        """
+        Check if the daemon is running. If not, start the daemon.
+        """
+        # FIXME: we have a concurrency issue here. If two processes start the daemon at the same time,
+        # they may both think the daemon is not running and start it. One solution is to double check
+        # whether the daemon is running when setting the pid file.
+        if self.deamonpid.is_running:
+            return
+
+        # XXX: here we use utils.runbg instead of os.fork, because using fork will inherit all the
+        # memory pages of the parent process, which is not what we want. It is possible that the parent
+        # process uses a lot of memory, while the daemon should be as light as possible.
+        utils.runbg(self.launch_command())
+
+    def stop(self):
+        """
+        Stop the daemon.
+        """
+        if not self.deamonpid.is_running:
+            return
+
+        os.kill(self.deamonpid.pid, signal.SIGTERM)
+        while self.deamonpid.is_running:
+            time.sleep(1)
+        del self.deamonpid.pid
+
+    def loop(self, ppid: int):
+        """
+        Check the jobs every interval seconds. The loop only starts when the parent process exits.
+        """
+        # take down the pid of the current process
+        self.deamonpid.pid = os.getpid()
+
+        # wait for the parent process to exit
+        while (Path('/proc') / str(ppid)).exists():
+            time.sleep(self.interval)
+
+        # start the loop
+        while True:
+            jobs = [(service, job) for service in self.service_manager.services.values() for job in service.jobs]
+            jids = [(service, job, jid) for service, job in jobs if ((jid := self.getid(job)) is not None)]
+
+            if not jids:
+                break
+
+            for service, job, jid in jids:
+                if not self.validate(jid):
+                    self.service_manager.stop(service.env, job)
+
+            time.sleep(self.interval)
+
+        # remove the pid file
+        del self.deamonpid.pid
+        exit(0)
+
+
+class ProcessDaemon(Daemon):
+    """
+    The process daemon that manages the jobs based on the process id.
+    """
+    name = 'pid'
+
+    def launch_command(self) -> List[str]:
+        """
+        Get the command to launch the daemon.
+        """
+        return [
+            sys.executable, # the python interpreter
+            "-c",
+            "from slash.daemon import ProcessDaemon; ProcessDaemon().loop({})".format(
+                os.getpid()
+            ),
+        ]
+
+    def getid(self, job: str) -> str:
+        """
+        Get the unique identifier of the job. It will be passed to the validate method to check if the job is dead.
+        If the job is beyond the control of the daemon, return None.
+        """
+        match = re.match(r"^__pid_(?P<pid>\d+)_(?P<comment>\w+)__$", job)
+        return None if not match else match.group("pid")
+
+    def validate(self, jid: str) -> bool:
+        """
+        Validate the existence of a job.
+        """
+        return (Path('/proc') / jid).exists()
--- slash/slash.py
+++ slash/slash.py
@@ -1,187 +1,12 @@
-from typing import Dict, List
-from pathlib import Path
-import re
 import os
-import time
-import json
-import filelock
-import sys
-import signal
-from slash.core import Env, EnvsManager, ServiceManager, Service, WORK_DIR
+from typing import Dict
+
 import slash.utils as utils
+from slash.core import Env, EnvsManager, Service, ServiceManager
+from slash.daemon import ProcessDaemon
+
 
 logger = utils.logger
-
-
-class DaemonPid:
-    def __init__(self, name: str):
-        self.name = name
-
-    @property
-    def deamon_pid(self) -> Path:
-        return WORK_DIR / 'daemon.pid'
-    
-    @property
-    def pid(self) -> int:
-        with filelock.SoftFileLock(self.deamon_pid.with_suffix('.lock')):
-            if self.deamon_pid.exists():
-                with open(self.deamon_pid, 'r') as f:
-                    data = json.load(f)
-                return data.get(self.name, None)
-            else:
-                with open(self.deamon_pid, 'w') as f:
-                    json.dump({}, f)
-        return None
-    
-    @pid.setter
-    def pid(self, pid: int):
-        with filelock.SoftFileLock(self.deamon_pid.with_suffix('.lock')):
-            with open(self.deamon_pid, 'r') as f:
-                data = json.load(f)
-            data[self.name] = pid
-            with open(self.deamon_pid, 'w') as f:
-                json.dump(data, f)
-
-    @pid.deleter
-    def pid(self):
-        with filelock.SoftFileLock(self.deamon_pid.with_suffix('.lock')):
-            with open(self.deamon_pid, 'r') as f:
-                data = json.load(f)
-            if self.name in data:
-                del data[self.name]
-            with open(self.deamon_pid, 'w') as f:
-                json.dump(data, f)
-    
-    @property
-    def is_running(self):
-        if not (ret := self.pid is not None and (Path('/proc') / str(self.pid)).exists()):
-            del self.pid
-        return ret
-
-
-class Daemon:
-    """
-    The abstract daemon class that manages the services.
-
-    The daemon will be started when the slash object is created, it will take down the pid of the
-    current process; after the process exits, the daemon will check the jobs corresponding to the
-    its type (shell, with, ...) and remove the dead ones every 60 seconds. If there are no jobs left,
-    the daemon will exit.
-    """
-    # the identifier of the daemon, should be unique
-    name = None
-    # the loop interval (seconds)
-    interval = 60
-
-    def __init__(self):
-        self.envs_manager = EnvsManager()
-        self.service_manager = ServiceManager(self.envs_manager)
-        self.deamonpid = DaemonPid(self.name)
-
-    def launch_command(self) -> List[str]:
-        """
-        Get the command to launch the daemon. Should be implemented by the subclass.
-        """
-        raise NotImplementedError
-
-    def getid(self, job: str) -> str:
-        """
-        Get the unique identifier of the job. It will be passed to the validate method to check if the job is dead.
-        If the job is beyond the control of the daemon, return None. Should be implemented by the subclass.
-        """
-        raise NotImplementedError
-
-    def validate(self, jid: str) -> bool:
-        """
-        Validate the existence of a job. Should be implemented by the subclass.
-        """
-        raise NotImplementedError
-
-    def start(self):
-        """
-        Check if the daemon is running. If not, start the daemon.
-        """
-        if self.deamonpid.is_running:
-            return
-
-        # XXX: here we use utils.runbg instead of os.fork, because using fork will inherit all the
-        # memory pages of the parent process, which is not what we want. It is possible that the parent
-        # process uses a lot of memory, while the daemon should be as light as possible.
-        utils.runbg(self.launch_command())
-
-    def stop(self):
-        """
-        Stop the daemon.
-        """
-        if not self.deamonpid.is_running:
-            return
-
-        os.kill(self.deamonpid.pid, signal.SIGTERM)
-        while self.deamonpid.is_running:
-            time.sleep(1)
-        del self.deamonpid.pid
-
-    def loop(self, ppid: int):
-        """
-        Check the jobs every interval seconds. The loop only starts when the parent process exits.
-        """
-        # take down the pid of the current process
-        self.deamonpid.pid = os.getpid()
-
-        # wait for the parent process to exit
-        while (Path('/proc') / str(ppid)).exists():
-            time.sleep(self.interval)
-
-        # start the loop
-        while True:
-            jobs = [(service, job) for service in self.service_manager.services.values() for job in service.jobs]
-            jids = [(service, job, jid) for service, job in jobs if ((jid := self.getid(job)) is not None)]
-
-            if not jids:
-                break
-
-            for service, job, jid in jids:
-                if not self.validate(jid):
-                    self.service_manager.stop(service.env, job)
-
-            time.sleep(self.interval)
-        
-        # remove the pid file
-        del self.deamonpid.pid
-        exit(0)
-
-
-class ProcessDaemon(Daemon):
-    """
-    The process daemon that manages the jobs based on the process id.
-    """
-    name = 'pid'
-
-    def launch_command(self) -> List[str]:
-        """
-        Get the command to launch the daemon.
-        """
-        return [
-            sys.executable, # the python interpreter
-            "-c",
-            "from slash.slash import ProcessDaemon; ProcessDaemon().loop({})".format(
-                os.getpid()
-            ),
-        ]
-
-    def getid(self, job: str) -> str:
-        """
-        Get the unique identifier of the job. It will be passed to the validate method to check if the job is dead.
-        If the job is beyond the control of the daemon, return None.
-        """
-        match = re.match(r"^__pid_(?P<pid>\d+)_(?P<comment>\w+)__$", job)
-        return None if not match else match.group("pid")
-
-    def validate(self, jid: str) -> bool:
-        """
-        Validate the existence of a job.
-        """
-        return (Path('/proc') / jid).exists()
 
 
 class Slash:
@@ -198,7 +23,7 @@ class Slash:
         Launch a job.
         """
         return self.service_manager.launch(self.env, job)
-    
+
     def stop(self, job: str) -> None:
         """
         Stop the service.
@@ -215,7 +40,7 @@ class Slash:
         if is_updated and service is not None:
             service.update()
         return is_updated
-    
+
     def info(self) -> Dict[str, str]:
         """
         Get the environment information.
@@ -234,7 +59,7 @@ class Slash:
             info['service_dashboard'] = service.get_controller_urls()
         else:
             info['service_status'] = 'Offline'
-        
+
         return info
 
     @classmethod
@@ -247,7 +72,7 @@ class Slash:
             file (str): The path to the environment file, or the link to the subscription.
         """
         EnvsManager().create_env(name, file)
-    
+
     @classmethod
     def remove(cls, name: str) -> None:
         """
@@ -264,14 +89,14 @@ class Slash:
         List all environments.
         """
         return EnvsManager().get_envs()
-    
+
     def __enter__(self) -> 'Slash':
         service = self.launch("__pid_{pid}_with__".format(pid=os.getpid()))
         self._old_envs = (os.environ.get('http_proxy', None), os.environ.get('https_proxy', None))
         os.environ['http_proxy'] = f"http://127.0.0.1:{service.port}"
         os.environ['https_proxy'] = f"http://127.0.0.1:{service.port}"
         return self
- 
+
     def __exit__(self, type, value, trace) -> None:
         self.stop("__pid_{pid}_with__".format(pid=os.getpid()))
         if self._old_envs[0] is not None:
